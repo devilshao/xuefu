4e561dc6cbbfba47ce677af524782d48f6eadc09
devilshao <shaochong1989@126.com>
2020-05-31 18:46:46 +0800

    新建项目

---
+++ src/main/java/com/sc/xuefu/XuefuApplication.java
@@ -0,0 +1,13 @@
+package com.sc.xuefu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuefuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuefuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sc/xuefu/XuefuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.xuefu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuefuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
