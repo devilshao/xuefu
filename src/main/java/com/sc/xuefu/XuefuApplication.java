07d45794ba2a1129f6761ddc7877379856ffe024
devilshao <shaochong1989@126.com>
2020-05-31 19:52:11 +0800

    第一次修改項目

--- src/main/java/com/sc/xuefu/XuefuApplication.java
+++ src/main/java/com/sc/xuefu/XuefuApplication.java
@@ -2,8 +2,9 @@ package com.sc.xuefu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class XuefuApplication {
 
     public static void main(String[] args) {
